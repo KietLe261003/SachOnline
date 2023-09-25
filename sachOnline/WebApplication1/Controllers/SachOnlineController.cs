a1622ec73c28c95b4dd49aac2ae3f7bcb570be82
KietLe2610 <letuankiet26102003kl@gmail.com>
2023-09-25 14:43:19 +0700

    haha

--- sachOnline/WebApplication1/Controllers/SachOnlineController.cs
+++ sachOnline/WebApplication1/Controllers/SachOnlineController.cs
@@ -3,21 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebApplication1.Models;
 namespace WebApplication1.Controllers
 {
     public class SachOnlineController : Controller
     {
+        QLBanSachEntities db = new QLBanSachEntities();
+        private List<SACH> Laysachmoi(int count)
+        {
+            return db.SACHes.OrderByDescending(item => item.NgayCapNhat).Take(count).ToList();
+        }
         // GET: SachOnline
         public ActionResult Index()
         {
-            return View();
+            var Danhsach = Laysachmoi(6);
+            return View(Danhsach);
         }
         public ActionResult ChuDePartial()
         {
-            return PartialView();
+            var listcd = db.CHUDEs.ToList();
+            var listnxb = db.NHAXUATBANs.ToList();
+
+            ViewBag.nxb = listnxb;
+            return PartialView(listcd);
+        }
+        private List<SACH> LaySachBanNhieu(int count=6)
+        {
+            return db.SACHes.OrderByDescending(item => item.SoLuongBan).Take(count).ToList();
         }
         public ActionResult SachBanNhieu()
+        {
+            var ds = LaySachBanNhieu(6);
+            return PartialView(ds);
+        }
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult FooterPartial()
         {
             return PartialView();
         }
